9f88aa335e1aa5fc6e3ac8ea143cbf3db8c6168d
dacalderonp <dacalderonp@protonmail.com>
2024-02-13 13:44:54 -0500

    corrigiendo errores

--- src/proyecto/RobotLexer.java
+++ src/proyecto/RobotLexer.java
@@ -31,7 +31,7 @@ public class RobotLexer {
 
     private void tokenize(String line) {
         // la expresiones regulares para simplificar la sintaxis
-        Pattern commandPattern = Pattern.compile("\\(|\\)|defvar|if|can-move\\?|move-dir|turn|not|blocked\\?|move|repeat|Spaces|isZero\\?|defun|put|pick|run-dirs");
+        Pattern commandPattern = Pattern.compile("\\(|\\)|defvar|if|can-move\\?|move-dir|turn|not|blocked\\?|move|repeat|Spaces|isZero\\?|defun|put|pick\\s.|run-dirs ");
         Pattern variablePattern = Pattern.compile("([a-zA-Z]+)"); // Variables
         Pattern numberPattern = Pattern.compile("\\d+"); // numeros
         Pattern constantPattern = Pattern.compile("(Dim|myXpos|myYpos|myChips|myBalloons|balloonsHere|ChipsHere|Spaces|left|:right|:around|:north|:south|:east|:west|:balloons|:chips|:front)"); // Constantes
@@ -44,7 +44,7 @@ public class RobotLexer {
         Matcher matcher = combinedPattern.matcher(line);
         
         while (matcher.find()) {
-            String token = matcher.group().toLowerCase(); // Convert to lowercase            
+            String token = matcher.group(); // Convert to lowercase            
             tokens.add(token);
         }
     }   
